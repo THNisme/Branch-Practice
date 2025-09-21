52819ace89521617841bfad25bd4d90fa5ab9340
THNisme <nghiath.ce191115@gmail.com>
2025-09-21 14:15:02 +0700

    BMI calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Wellcome = () => {
+    const [name, setName] = useState("NghiaTH");
+    const [message, setMessage] = useState(`Hello, wellcome to ${name}`);
 
-export default App;
+    const inputChange = (e) => setName(e.target.value);
+    const submitChange = () => setMessage(`Hello, wellcome to ${name}`);
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <h1>{message}</h1>
+
+            <input
+                type="text"
+                value={name}
+                onChange={inputChange}
+            >
+            </input>
+            <button onClick={submitChange}>Change Message</button>
+        </div>
+    );
+
+};
+
+export default Wellcome;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import InfoPage from './InforPage';
+import TaskC from './TaskC';
+import BMI from './BMI';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BMI />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
